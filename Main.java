bc3803779ac71bfb12aad70927a04e8ced461824
khrid0328 <khrid0328@wrdsb.ca>
2020-10-06 17:02:02 +0000

    A5Q2 This program prints the numbers from 1 to a number the user inputs on a single line with commas in between them

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program prints the numbers from 1 to a number the user inputs on a single line with commas in between them
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+
+    // create scanner for user inputs
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter a positive integer
+    System.out.println("Please enter a positive integer");
+
+    // create a variable for user input
+    int integer = input.nextInt();
+
+    // use a for loop to print all numbers from one to integer on a single line
+    for(int i = 1; i < integer; i++){
+      
+      // loop action: print the number
+      System.out.print(i + ", " );
+    }
+    System.out.print(integer);
   }
 }
